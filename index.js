18c50995d8104f19ae6f9170c8aaa3fdc1cac61b
Harpreet Ghotra <harpreetghotra40@icloud.com>
2019-07-15 16:25:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, eventName){
+    let arr =[];
+    for (let x = 0 ; x < names.length ; x++){
+        arr.push("Thank you, " + names[x] + ", for the wonderful surprise gift!");
+    }
+    return arr;
+}
+
+function countdown(count){
+    for (let i = count ; i >= 0 ; i--){
+        console.log(i)
+    }
+}
